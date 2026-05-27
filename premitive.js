7b6c3f43a61465166ce3b938524fb7895c73caa1
MD. Mirazul Islam Shourov <shourovsc38@gmail.com>
2026-05-27 18:10:14 +0600

    Refactor premitive.js to include student object

--- premitive.js
+++ premitive.js
@@ -4,4 +4,15 @@ console.log(typeof string) //string
 
 
 //Boolean
-let hasCar = false, hasMarried = false 
+let hasCar = false, hasMarried = false ;
+
+//object
+let student ={
+  name : "Shourov", 
+  age : 23, 
+  hasCar : false, 
+  skill :{}, 
+  frontend :[], 
+  backend :[] 
+  
+} 
