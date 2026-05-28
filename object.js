31607134b3f663194250b3f1630b7894bf365d75
MD. Mirazul Islam Shourov <shourovsc38@gmail.com>
2026-05-28 15:45:22 +0600

    Add developer object with skills and personal info
    Added a developer object with personal and skill details.

--- object.js
+++ object.js
@@ -5,3 +5,21 @@ const student = {
 
 console.log(student.name)  //Shourov
 console.log(student['name'])  //Shourov
+
+
+
+//object
+let developer ={
+  name : "Shourov", 
+  age : 23, 
+  hasCar : false, 
+  skill :{
+    frontend : true, 
+    backend : true
+  }, 
+  frontend :["HTML5", "CSS3", "ES6", "TS", "TAILWIND CSS", "React.js", "Next.js", "Bootstrap" ], 
+  backend :["Node.js", "Express.js" ], 
+  database : ["MongoDB", "PostgresSQL" ], 
+  authentication :["NextAuth", "JWT", "Firebase"] 
+  
+} 
